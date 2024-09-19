9212eb516fbd5ad7c257f9fb506949772518ede8
Cesar Ricardo Tavares <63663846+CesarTavares@users.noreply.github.com>
2024-09-19 17:38:31 +0000

    modificando a frase novamente

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi Familia este é o meu primeiro Deploy.!!!!:)</h1>
+    return <h1>Oi Familia este é o meu primeiro Deploy em produção.!!!!:)</h1>
 }
 
 export default Home;
