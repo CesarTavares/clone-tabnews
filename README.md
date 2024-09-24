a84df799de40a960f00f905070cf001f2428e1a9
Cesar Ricardo Tavares <63663846+CesarTavares@users.noreply.github.com>
2024-09-24 15:01:28 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 implementação do curso.dev aula de git
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Oi Familia este é o meu primeiro Deploy em produção.!!!!:)</h1>
+  return <h1>Oi Familia este é o meu primeiro Deploy em produção.!!!!:)</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("meu teste 2");
 }
 
 export default Home;
