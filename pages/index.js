46a341bdd6123e9703c8a2e2e3e6bf98b8a05e70
Cesar Ricardo Tavares <63663846+CesarTavares@users.noreply.github.com>
2024-09-26 13:11:52 +0000

    excluido as funções teste,1 e 2

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,4 @@
 function Home() {
   return <h1>Oi Familia este é o meu primeiro Deploy em produção.!!!!:)</h1>;
 }
-
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("meu teste 2");
-}
-
 export default Home;
